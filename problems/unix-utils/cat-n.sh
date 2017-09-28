bd57b6bb7a1e6d8ed67a25d41ea3ab929e3ea651
Bryce Richard Kirkpatrick <brycekirkpatrick@email.arizona.edu>
2017-09-28 11:16:10 -0700

    all good

--- problems/unix-utils/cat-n.sh
+++ problems/unix-utils/cat-n.sh
@@ -1,25 +1,25 @@
 
 #!/bin/bash
-FILE=$1
-if [[ $# -eq 0 ]]; then
+FILE=$1  #set variable for file, input argument 1
+if [[ $# -eq 0 ]]; then #testing if the argument is present
 
-        printf "Usage: %s FILE\n" "$(basename "$0")"
+        printf "Usage: %s FILE\n" "$(basename "$0")" #print usage statment if no argument present
         exit 1
 else
 
 
-    	if [[ -f "$FILE" ]]; then
-i=0
+    	if [[ -f "$FILE" ]]; then #testing if FILE (arg 1) is in fact a file
+i=0 #setting base variable for while loop
 
-                while read -r LINE; do
-                let i++
+                while read -r LINE; do #while reading the lines from the input
+                let i++			#add i+1 for each iteration
 
-                printf "$i $LINE\n"
+                printf "$i $LINE\n" #print the value for i then the first component of input argument
 
-                 done<"$FILE"
+                 done<"$FILE" 
 
         else
-		printf "\"%s\" is not a file\n" "$(basename "$FILE")"
+		printf "\"%s\" is not a file\n" "$(basename "$FILE")" #if the file test fails, print error message
 		        exit 1
 	fi
 
--- problems/unix-utils/head.sh
+++ problems/unix-utils/head.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-FILE=$1
-NUM=$2
-if [[ $# -eq 0 ]]; then
-        printf "Usage: %s FILE [NUM]\n" "$(basename "$0")"
+FILE=$1 #define var for reference in loops
+NUM=$2 #define car for reference in loops
+if [[ $# -eq 0 ]]; then # If there is no argument input 
+        printf "Usage: %s FILE [NUM]\n" "$(basename "$0")" #print usage statement 
         exit 1
 else
-if [[ -f "$FILE" ]]; then
-if [[ $NUM -gt 0 ]]; then
+if [[ -f "$FILE" ]]; then    #-f is testing to see if input for FILE is a file
+if [[ $NUM -gt 0 ]]; then    #if NUM has input 
 
         i=0
 
-	while read -r LINE; do
-                let i++
-                       	if [[ i -le $NUM ]]; then
-				echo $LINE
+	while read -r LINE; do #for each line
+                let i++		#add i + 1	
+                       	if [[ i -le $NUM ]]; then #if i is less than the NUM
+				echo $LINE	#print the component of the line
                        	
                         else
 
@@ -25,7 +25,7 @@ if [[ $NUM -gt 0 ]]; then
 else
     	i=0
 
-	while read -r LINE; do
+	while read -r LINE; do #see above, this is our default counter for 3 lines
         let i++
                	if [[ i -le 3 ]]; then
               	 	 echo $LINE
@@ -37,7 +37,7 @@ else
         done < "$FILE"
 fi
 else
-    	printf "\"%s\" is not a file\n" "$(basename "$FILE")"
+    	printf "\"%s\" is not a file\n" "$(basename "$FILE")" #if file test fails i.e. no file, print error statement
 	exit 1		
 fi
 fi
