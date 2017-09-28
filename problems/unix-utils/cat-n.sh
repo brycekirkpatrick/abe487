af7f71692d97fb33ad6a75486be2d05780546df7
Bryce Richard Kirkpatrick <brycekirkpatrick@email.arizona.edu>
2017-09-27 23:25:47 -0700

    all good

--- problems/unix-utils/cat-n.sh
+++ problems/unix-utils/cat-n.sh
@@ -1,3 +1,29 @@
-#!/bin/bash
 
-echo "OK"
+#!/bin/bash
+FILE=$1
+if [[ $# -eq 0 ]]; then
+
+        printf "Usage: %s FILE\n" "$(basename "$0")"
+        exit 1
+else
+
+
+    	if [[ -f "$FILE" ]]; then
+i=0
+
+                while read -r LINE; do
+                let i++
+
+                printf "$i $LINE\n"
+
+                 done<"$FILE"
+
+        else
+		printf "\"%s\" is not a file\n" "$(basename "$FILE")"
+		        exit 1
+	fi
+
+
+fi
+
+
--- problems/unix-utils/head.sh
+++ problems/unix-utils/head.sh
@@ -1,3 +1,43 @@
 #!/bin/bash
 
-echo "OK"
+FILE=$1
+NUM=$2
+if [[ $# -eq 0 ]]; then
+        printf "Usage: %s FILE [NUM]\n" "$(basename "$0")"
+        exit 1
+else
+if [[ -f "$FILE" ]]; then
+if [[ $NUM -gt 0 ]]; then
+
+        i=0
+
+	while read -r LINE; do
+                let i++
+                       	if [[ i -le $NUM ]]; then
+				echo $LINE
+                       	
+                        else
+
+                        break
+                        fi
+                        done < "$FILE"
+        exit 1
+else
+    	i=0
+
+	while read -r LINE; do
+        let i++
+               	if [[ i -le 3 ]]; then
+              	 	 echo $LINE
+                	
+                else
+
+                break
+                fi
+        done < "$FILE"
+fi
+else
+    	printf "\"%s\" is not a file\n" "$(basename "$FILE")"
+	exit 1		
+fi
+fi
