1c64ff072670b756807b47293575503a7709e33a
Bryce Richard Kirkpatrick <brycekirkpatrick@email.arizona.edu>
2017-10-04 15:15:54 -0700

    homework done

--- problems/hello-py/hello.py
+++ problems/hello-py/hello.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
-"""say hello to the arguments"""
 
-print('OK')
+import sys
+
+args=sys.argv
+
+if len(args) < 2:
+	print('Usage:', args[0], 'NAME')
+	sys.exit(1)
+elif len(args) == 2:
+        print('Hello to the {} of you: {}!'.format(len(args)-1, args[1]))
+elif len(args) == 3:
+	print('Hello to the {} of you: {} and {}!'.format(len(args)-1, args[1], args[2]))
+elif len(args) > 3:
+	print('Hello to the {} of you: {}, and {}!'.format(len(args)-1,', '.join(args[1:-1]), args[-1]))
+else:
+	sys.exit(1)
+
--- problems/hello-py/vowel_counter.py
+++ problems/hello-py/vowel_counter.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
-"""count the vowels in a word"""
 
-print('OK')
+import os
+import sys
+
+args = sys.argv
+
+if len(args) < 2:
+	print('Usage: {} STRING'.format(os.path.basename(args[0])))
+	sys.exit(1)
+else:
+	z=0  #not using i, worrried I will get the vowels and the count confused
+	for letter in args[1]: 
+		if letter in 'aeiou':
+			z+=1
+	if z == 1:
+		print('There is 1 vowel in "{}."'.format(args[1]))
+	elif z > 1:
+		print('There are {} vowels in "{}."'.format(z,args[1]))
+	else:
+		print('There are 0 vowels in "{}."'.format(args[1]))
