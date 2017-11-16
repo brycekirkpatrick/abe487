7d463ecf1dd3d020340e40929b52024378f48edc
Bryce Richard Kirkpatrick <brycekirkpatrick@email.arizona.edu>
2017-11-16 09:33:54 -0700

    adding parse-blast

--- problems/parse-blast/parse_blast_tab.py
+++ problems/parse-blast/parse_blast_tab.py
@@ -1,3 +1,47 @@
 #!/usr/bin/env python3
 
-print('OK')
+import argparse
+import os
+import sys
+#from Bio import SearchIO
+
+# --------------------------------------------------
+def get_args():
+    parser = argparse.ArgumentParser(description='Parse BLAST tab')
+    parser.add_argument('positional', metavar='file', help='BLAST tab output')
+    parser.add_argument('-p', '--pct_id', help='Percent identity',
+                        metavar='float', type=float, default=0)
+    parser.add_argument('-e', '--evalue', help='',
+                        metavar='float', type=float, default=None)
+    return parser.parse_args()
+
+# --------------------------------------------------
+def main():
+    args = get_args()
+    file = args.positional
+    pct_id = args.pct_id
+    evalue = args.evalue
+
+#    info= SearchIO.read(file, 'blast-tab')
+#    line = 'qid\tsid\t83.99\t37\t14\t15\t1\t147\t1\t149\t0.0\t219\n'    
+#    for stuff in info:
+#        print(Hsp(line).pident)
+#    hsp=Hsp(line)
+    flds = 'qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore'.split()
+#    f_dict= {}
+#    i=0
+    for line in open(file):
+        row=dict(zip(flds , line.split('\t')))
+        attrs={}
+        #print(row)
+        if float(row['pident']) < pct_id:
+            continue   
+        if evalue is not None and float(row['evalue']) > evalue: 
+            continue
+        print('\t'.join([row['qseqid'], row['sseqid'], row['pident'], row['evalue']]))
+         
+#        print('ok')
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
